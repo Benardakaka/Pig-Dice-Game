97baab2a35f176e0cafe4f1982b2aa35f6436fe9
Benardakaka <benardakaka484@gmail.com>
2020-10-23 09:22:45 +0300

    add README.md

---
+++ README.md
@@ -0,0 +1,34 @@
+## PIG-DICE-GAME
+[Check it out]("https://github.com/Benardakaka/Pig-Dice-Game")
+
+
+## Author:
+# BENARD AKAKA
+## Description: 
+A Pig-Dice-Game web application.
+
+
+
+## Technologies:
+* HTML
+* CSS
+* Bootstrap.min.css
+* Javascript
+* Jquery
+
+## BDD:
+| Inputs |  Description |
+| :---         |          ---: |
+| Roll on  | ` 5 times`|
+| 1st player     | ` Benard`   |
+| 2rd player    | ` Mercy`   |
+| 3rd player    | | ` Kerry`
+| Total Scores    | ` 100`  |
+| Number of player   | ` 2`   |
+| Registration mode   | ` No register for the game`   |
+
+## License:
+>MIT LICENSE ccc
+
+## Collaborate:
+To collaborate with Pig-Dice-Game, reach on " benardakaka484@gmail.com "
--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,124 @@ function Player(userName,score) {
   Player.prototype.totalScore = function (number) {
     return this.score += number;
   };
+
+  var playerArray = [];
+  var rollArray = [0];
+  
+  function rollDice(number) {
+    return Math.floor(Math.random() * Math.floor(number)) + 1;
+  }
+  
+  function getSum(total, number) {
+    return total + number;
+  }
+  
+  function rollArrayTotal(sum) {
+    return rollArray.reduce(sum)
+  }
+  
+  function runningRollDice() {
+    rollArray.push(rollDice(6))
+    return rollArrayTotal(getSum);
+  }
+  
+  function emptyRollArray() {
+   return rollArray = [0];
+  }
+  
+  function reset() {
+    for (var i = 0; i < playerArray.length; i++) {
+      playerArray[i].score = 0;
+    }
+  }
+  
+  //User Logic
+  $(function(){
+    $("#player1-form").submit(function(event){
+      event.preventDefault();
+      var p1Name = $("#p1InputName").val();
+      var playerObject = new Player(p1Name, 0);
+      var computerObject = new Player("Mercy", 0);
+      playerArray.push(playerObject, computerObject);
+      $("#player1-name").text(p1Name);
+      $("#player1-scorecard").show();
+      $("#player2-name").text("Mercy");
+      $("#player2-scorecard").show();
+      $("#player1-submitcard").show();
+    });
+  
+    $("#roll-button").click(function(){
+      var p1CurrentRunningTotal = runningRollDice();
+      var currentRollPlayer= rollArray[rollArray.length-1];
+      console.log(rollArray);
+      if (currentRollPlayer === 1) {
+        $("#p1runningTotal").text(0);
+        emptyRollArray();
+        //computer turn
+        for (var i = 0; i < 2; i++) {
+          runningRollDice();
+          var currentRollComputer= rollArray[rollArray.length-1];
+          console.log(rollArray);
+          if (currentRollComputer === 1) {
+            emptyRollArray();
+            i=2;
+            $("#p2runningTotal").text(0);
+          } else {
+            $("#p2runningTotal").text(rollArrayTotal(getSum));
+          }
+        }
+        playerArray[1].totalScore(rollArrayTotal(getSum));
+        $("#p2scoreTotal").text(playerArray[1].score);
+        emptyRollArray();
+        if (playerArray[1].score >= 100) {
+          alert("Sorry " + playerArray[0].userName + ", the winner is " + playerArray[1].userName + ". Better luck next time!");
+          $("#reset-btn").show();
+        }
+      } else {
+        $("#p1runningTotal").text(p1CurrentRunningTotal);
+      }
+      $("#rollDisplay p").text(currentRollPlayer);
+      $("#player1-scorecard").show();
+      $("#rollDisplay").show();
+    });
+  
+    $("#hold-button").click(function(){
+      playerArray[0].totalScore(rollArrayTotal(getSum));
+      $("#p1scoreTotal").text(playerArray[0].score);
+      emptyRollArray();
+      $("#p1runningTotal").text(0);
+      if (playerArray[0].score >= 100) {
+        alert("congratulations " + playerArray[0].userName + ", you are winner!");
+        $("#reset-btn").show();
+      }
+      //computer turn
+      for (var i = 0; i < 2; i++) {
+        runningRollDice();
+        var currentRollComputer= rollArray[rollArray.length-1];
+        console.log(rollArray);
+        if (currentRollComputer === 1) {
+          emptyRollArray();
+          i=2;
+          $("#p2runningTotal").text(0);
+        } else {
+          $("#p2runningTotal").text(rollArrayTotal(getSum));
+        }
+      }
+      playerArray[1].totalScore(rollArrayTotal(getSum));
+      $("#p2scoreTotal").text(playerArray[1].score);
+      emptyRollArray();
+      if (playerArray[1].score >= 100) {
+        alert("Sorry " + playerArray[0].userName + ", the winner is " + playerArray[1].userName + ". Better luck next time!");
+        $("#reset-btn").show();
+      }
+    });
+  
+    $("#reset-btn").click(function(e){
+      e.preventDefault();
+      reset();
+      console.log(playerArray[1].score);
+      $("#p1scoreTotal").text(playerArray[0].score);
+      $("#p2scoreTotal").text(playerArray[1].score);
+    });
+  });
+  
